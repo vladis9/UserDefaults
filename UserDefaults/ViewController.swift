cb1302d09d9b746e6e2c770fd7201ac37ca7b7e4
Vlad Zinevich <vladisznv@gmail.com>
2021-09-11 10:06:05 +0300

    1. setup button 2. save user defaults 3. load user defaults

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var secondNameTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        userNameLabel.isHidden = true
+        
+        if let userName = UserDefaults.standard.value(forKey: "userName") {
+            userNameLabel.isHidden = false
+            userNameLabel.text = userName as? String
+        }
     }
 
+    @IBAction func donePressed() {
+        
+        guard let firstName = firstNameTextField.text, !(firstName.isEmpty) else {
+            wrongFormatAlert()
+            return
+        }
+        
+        guard let secondName = secondNameTextField.text, !(secondName.isEmpty) else {
+            wrongFormatAlert()
+            return
+        }
+        
+        if let _ = Double(firstName) {
+            wrongFormatAlert()
+        } else if let _ = Double(secondName) {
+            wrongFormatAlert()
+        } else {
+            userNameLabel.isHidden = false
+            userNameLabel.text = firstName + " " + secondName
+            UserDefaults.standard.set(userNameLabel.text, forKey: "userName")
+        }
+        
+        firstNameTextField.text = nil
+        secondNameTextField.text = nil
+    }
+}
 
+extension ViewController {
+    
+    private func wrongFormatAlert() {
+        let alert = UIAlertController(
+            title: "Wrong Format",
+            message: "Please enter your name",
+            preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
